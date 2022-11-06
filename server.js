79ac6ec8691d6e362ef25b7d6d1c523672371e97
ea <64137287+emreaksel@users.noreply.github.com>
2022-11-06 22:25:35 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -8,26 +8,56 @@ npm i express
 manuel başlamak için node server.js
 nodemon ile başlamak için npm start
 
+mongodb bağlantısı için https://app.patika.dev/courses/nodejs/HerokuDeployment
+mongodb atesiask P1GHu5R4tBpRDj9j
+heroku da yayımlamak için https://www.youtube.com/watch?v=skGNRkosnQU&ab_channel=MertStack
 */
 
 import express from 'express';
+import { MongoClient } from 'mongodb'
+
+const url = "mongodb+srv://atesiask:P1GHu5R4tBpRDj9j@cluster0.8x77kki.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(url);
+
+/* async function main() {
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db("atesiask");
+    const collection = db.collection('documents');
+
+    const insertResult = await collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }]);
+    console.log('Inserted documents =>', insertResult);
+
+    return 'done.';
+}
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close()); */
+
+
 var app = express();
 import path from 'path';
-import { foo, getirXML } from './public/js/getir_xml.js'; //import * as x from './public/js/getir_xml.js'  her şeyi getir
-const __dirname = path.resolve();
+import { foo, getirXML, getirNukte, getirFotograf } from './public/js/getir_xml.js'; //import * as x from './public/js/getir_xml.js'  her şeyi getir
+//const __dirname = path.resolve();
 
 const degiskenismi = { id: 50254, title: "Ateş-i Aşk", isim: "emre" };
 
 app.set('view engine', 'ejs'); //home.ejs yazmak yerine tüm sayfalar ejs olarak oluşturulacak demek istiyor
-app.use(express.static(__dirname + '/public')); //css, js, images flasörlerini saklamak için
+app.use(express.static(path.resolve() + '/public')); //css, js, images flasörlerini saklamak için
 //foo();
 app.get("/", async function (req, res) {
     //var dinlemelistesi=xml.map();
     //console.log(`Xml: ${xml.dinlemelistesi}`);
-    
-    var xml_listesi=await getirXML();
+
+    var xml_listesi = await getirXML();
+    var nukte_listesi = await getirNukte();
+    var fotograf_listesi = await getirFotograf();
     //console.log("axel: ",xml_listesi);
-    degiskenismi.xml_listesi=xml_listesi;
+    degiskenismi.xml_listesi = xml_listesi;
+    degiskenismi.nukte_listesi = nukte_listesi;
+    degiskenismi.fotograf_listesi = fotograf_listesi;
     res.render("atesiask", degiskenismi); //    ./sayfalar/home.ejs yapmıştım ama illa ki views klasörü istedi, kütüphane direkt views içine bakıyor
 });
 app.get("/test", async function (req, res) {
@@ -38,7 +68,7 @@ app.get("*", function (req, res) { //anlamsız talepler için
 });
 
 const PORT = 9000;
-var server = app.listen(process.env.PORT ||PORT, function () {
+var server = app.listen(process.env.PORT || PORT, function () {
     console.log(`Server is listening at localhost on port ${PORT}`);
 });
 
